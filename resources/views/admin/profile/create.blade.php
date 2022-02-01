78ed392c3d842eedbac35eeadcb5576efcb864d4
zonocan <zonocan.y.m@icloud.com>
2022-02-01 14:42:11 +0000

    my news & my profile created

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,17 @@
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'ニュースの新規作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
